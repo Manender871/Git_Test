233640aad6f5780737dd18c5156792e08bfee731
Manu <manunegi.ece@gmail.com>
2024-08-23 20:25:37 +0530

    American develop team

--- reset-password.py
+++ reset-password.py
@@ -8,3 +8,7 @@ if username == "Admin" and password == "Admin":
     print("You can reset")
 else:
     print("You cannot reset")
+if username == "Admin" and password == "Admin":
+    print("You can reset")
+else:
+    print("You cannot reset")
